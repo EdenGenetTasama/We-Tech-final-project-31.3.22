6dde43076011902433653b7d9803fe0169f05963
Amir-Mangisto <mangisto.man@gmail>
2022-03-09 19:59:52 +0200

    fix conflict

--- server/server.js
+++ server/server.js
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
 require('dotenv').config();
 
 
 console.log(process.env.AMIR);
-=======
-require('./db/WeTechDB')
-const express = require('express');
-const app = express();
-require('dotenv').config();
-
-
-
-
-
-app.use('/' ,()=>{});
-
-
-app.listen(process.env.PORT,()=>{});
->>>>>>> main
