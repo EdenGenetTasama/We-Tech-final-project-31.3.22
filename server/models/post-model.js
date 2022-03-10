cabc9a320190cfe128d5e6c91c6696a98ca603e1
Amir-Mangisto <mangisto.man@gmail>
2022-03-10 18:46:10 +0200

    deleting duplicate files

--- server/models/post-model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const schema = mongoose.Schema;
-
-const Comments = new schema({},{ timestamps: true });
-
-module.exports = mongoose.model('Comment' , Comments)
