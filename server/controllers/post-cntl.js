76c75f84a4b6f5df4288cb13a3cc87dac3155dcc
Amir-Mangisto <mangisto.man@gmail>
2022-03-10 18:42:59 +0200

    adding post crud and need to finish

---
+++ server/controllers/post-cntl.js
@@ -0,0 +1,63 @@
+const posts = require('../models/posts-model');
+
+
+module.exports = {
+    getAllPosts : async (req,res) =>{
+        await posts.find()
+        .then(post => res.status(200).json(post))
+        .catch(err => res.status(500).json(err))
+    },
+    getPostById : async (req,res) =>{
+        await posts.findById(req.params.id)
+        .then(post => res.status(200).json(post))
+        .catch(err => res.status(500).json(err))
+    },
+    addPost:async (req,res)=>{
+        await posts.create(req.body)
+        .then(post => res.status(200).json({message:"Post added successfully",post}))
+    },
+    updatePost : async (req,res)=>{
+        try{
+            const post = await posts.findById(req.params.id);
+            if(post.userId === req.body.userId){
+                await post.updateOne({$set: req.body})
+                res.status(200).json({message:"The post as been updated"})
+            }else{
+                res.status(403).json({message:"you can update only your post"})
+            }
+        }
+        catch(err){
+            res.status(500).json(err)
+        }
+    }
+    ,
+    deletePost: async (req,res)=>{
+        try{
+            const post = await posts.findById(req.params.id);
+            if(post.userId === req.body.userId){
+                await post.deleteOne();
+                res.status(200).json({message:"The post as been deleted"});
+            }else{
+                res.status(403).json({message:"you can delete only your post"});
+            }
+        }
+        catch(err){
+            res.status(500).json(err);
+        }
+    }
+    ,
+    likePost:async (req,res)=>{
+        try{
+            const post = await posts.findById(req.params.id);
+            if(!post.likes.includes(req.body.userId)){
+                await post.updateOne({$push: {likes: req.body.userId}});
+                res.status(200).json({message:"Post has been liked"});
+            }else{
+                await post.updateOne({$pull: {likes: req.body.userId}});
+                res.status(200).json({message:"Post has been disliked"});
+            }
+        }catch(err){
+            res.status(500).json(err);
+        }
+    }
+}
---
+++ server/routers/posts-route.js
@@ -0,0 +1,13 @@
+const postRouter = require('express').Router();
+const {getAllPosts, getPostById,addPost,updatePost,deletePost,likePost} = require('../controllers/post-cntl');
+
+postRouter.get('/',getAllPosts)
+postRouter.get('/:id',getPostById)
+postRouter.post('/',addPost)
+postRouter.put('/:id',updatePost)
+postRouter.delete('/:id',deletePost)
+postRouter.put('/id:/like',likePost)
+
+
+
+module.exports = postRouter;
