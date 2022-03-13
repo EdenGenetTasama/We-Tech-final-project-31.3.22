12755f5dba08eae223cfe1b261d638df76da8867
Amir-Mangisto <mangisto.man@gmail>
2022-03-13 19:37:04 +0200

    crating sidebar

--- client/src/components/pages/home/Home.jsx
+++ client/src/components/pages/home/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import Topbar from "../../parts/topbar/Topbar"
-
+import Topbar from "../../parts/topbar/Topbar";
+import Sidebar from '../../parts/sidebar/Sidebar';
+import Feed from '../../parts/feed/Feed';
+import Rightbar from '../../parts/rightbar/Rightbar';
+import './home.css';
 
 export default function Home() {
   return (
-    <div>
+    <>
         <Topbar/>
-    </div>
+        <div className='homeContainer'>
+          <Sidebar/>
+          <Feed/>
+          <Rightbar/>
+        </div>
+    </>
   )
 }
--- client/src/components/parts/topbar/Topbar.jsx
+++ client/src/components/parts/topbar/Topbar.jsx
@@ -29,11 +29,11 @@ export default function Topbar() {
             <span className="topbarIconBadge">2</span>
             <Notifications />
             <span className="topbarIconBadge">3</span>
-            <img
+            {/* <img
               src="https://www.meshek8.co.il/wp-content/uploads/2021/02/%D7%A6%D7%99%D7%9C%D7%95%D7%9D_%D7%A4%D7%A8%D7%95%D7%A4%D7%99%D7%9C_1.jpg"
               alt="profileImage"
               className="topbarImage"
-            />
+            /> */}
           </div>
         </div>
       </div>
