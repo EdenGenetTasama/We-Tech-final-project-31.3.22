0c94298bc5e63efadad352346e62bfabc2c1acc6
Amir-Mangisto <mangisto.man@gmail>
2022-03-13 19:43:19 +0200

    fixing home comp

--- client/src/components/pages/home/Home.jsx
+++ client/src/components/pages/home/Home.jsx
@@ -3,7 +3,6 @@ import Sidebar from '../../parts/sidebar/Sidebar';
 import Feed from '../../parts/feed/Feed';
 import Rightbar from '../../parts/rightbar/Rightbar';
 import './home.css';
-import Feed from "../../parts/feed/Feed";
 
 export default function Home() {
   return (
