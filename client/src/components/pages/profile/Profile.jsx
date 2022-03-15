cf52a4539af6b7130f150ee1321f4663c3b64b0b
Amir-Mangisto <mangisto.man@gmail>
2022-03-15 19:16:39 +0200

    FIXED PROFILE

--- client/src/components/pages/profile/Profile.jsx
+++ client/src/components/pages/profile/Profile.jsx
@@ -2,7 +2,7 @@ import Topbar from "../../parts/topbar/Topbar";
 import Sidebar from "../../parts/sidebar/Sidebar";
 import Feed from "../../parts/feed/Feed";
 import Rightbar from "../../parts/rightbar/Rightbar";
-import './profile.css'
+import "./profile.css";
 export default function Profile() {
   return (
     <>
@@ -12,17 +12,27 @@ export default function Profile() {
         <div className="profileRight">
           <div className="profileRightTop">
             <div className="profileCover">
-              <img className="profileCoverImg" src="/assets/persons/family-6398107_1920.jpg" alt="" />
-              <img className="profileUserImg" src="/assets/persons/3.jpg" alt="" />
+              <img
+                className="profileCoverImg"
+                src="/assets/persons/family-6398107_1920.jpg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="/assets/persons/3.jpg"
+                alt=""
+              />
             </div>
             <div className="profileInfo">
-                <h4 className="profileInfoName">Ella Mangolian</h4>
-                <span className="profileIDescription">Hello There</span>
+              <h4 className="profileInfoName">Ella Mangolian</h4>
+              <span className="profileIDescription">Hello There</span>
             </div>
           </div>
-          <div className="profileRightBottom"></div>
-          <Feed />
+          <div className="profileRightBottom">
+              <Feed />
           <Rightbar />
+          </div>
+        
         </div>
       </div>
     </>
