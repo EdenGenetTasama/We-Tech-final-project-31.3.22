d100afc8c86f637b4153c3586efac3441fbec247
haimaye074@gmail.com <haimaye074@gmail.com>
2022-03-15 19:08:45 +0200

    login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
-import Home from '../src/components/pages/home/Home.jsx';
-import Profile from './components/pages/profile/Profile.jsx'
+import React from "react";
+import "./App.css";
+import Home from "../src/components/pages/home/Home.jsx";
+import Profile from "./components/pages/profile/Profile.jsx";
+import Login from "./components/pages/login/Login.jsx";
+import Register from "./components/pages/register/Register";
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
       {/* <Profile/> */}
+      <Login />
     </div>
   );
 }
---
+++ client/src/components/pages/register/Register.jsx
@@ -0,0 +1,27 @@
+import "./Register.css";
+
+export default function Register() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+      <div className="loginLeft">
+        <h3 className="loginLogo">WeTechSocial</h3>
+        <span className="LoginDesc">
+            connect with friends and the world around you on WeTechSocial
+        </span>
+      </div>
+      <div className="loginRight">
+          <div className="loginBox">
+              <input placeholder="UserName" className="loginInput" />
+              <input placeholder="Email" className="loginInput" />
+              <input placeholder="Password" className="loginInput" />
+              <input placeholder="PasswordConfirm" className="loginInput" />
+              <button className="loginButton">Sign Up</button>
+              
+              <button className="loginRegisterButton">Log into Account</button>
+              </div>
+          </div>
+    </div>
+    </div>
+  );
+}
