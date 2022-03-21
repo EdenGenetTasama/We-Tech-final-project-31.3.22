057211ad52de79fb87c65b629a854e7c6cd28a99
Amir-Mangisto <mangisto.man@gmail>
2022-03-21 20:33:02 +0200

    add navigate to login and register

--- client/src/components/pages/register/Register.jsx
+++ client/src/components/pages/register/Register.jsx
@@ -1,15 +1,20 @@
 import axios from "axios";
 import { useRef } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "./Register.css";
 
 export default function Register() {
   const username = useRef();
+  const userLastname = useRef();
+  const profilePicture = useRef();
+  const coverPicture = useRef();
+  const from = useRef();
   const email = useRef();
+  const city = useRef();
   const password = useRef();
   const confirmPassword = useRef();
   const navigate = useNavigate();
- 
+
   const HandleClick = (e) => {
     e.preventDefault();
     if (confirmPassword.current.value !== password.current.value) {
@@ -17,16 +22,27 @@ export default function Register() {
     } else {
       const user = {
         userName: username.current.value,
+        userLastName: userLastname.current.value,
+        profilePicture: profilePicture.current.value,
+        coverPicture: coverPicture.current.value,
         email: email.current.value,
+        city: city.current.value,
         password: password.current.value,
-      }
-      try{
-         const res = axios.post("http://localhost:8800/auth/register", user);
-        navigate("/login")
+        from: from.current.value,
+      };
+      try {
+        const res = axios.post("http://localhost:8800/auth/register", user);
+        console.log(user);
+        navigate("/login");
       } catch (error) {
         console.log(error);
       }
     }
+  };
+
+  const reDirectToLogin = (e) => {
+    e.preventDefault();
+    navigate("/login");
   };
   return (
     <div className="login">
@@ -40,10 +56,32 @@ export default function Register() {
         <div className="loginRight">
           <form className="loginBox" onSubmit={HandleClick}>
             <input
-              placeholder="UserName"
+              placeholder="User Name"
               required
               ref={username}
               className="loginInput"
+            />
+
+            <input
+              placeholder="User Last Name"
+              required
+              ref={userLastname}
+              className="loginInput"
+            />
+
+            <input
+              placeholder="User Profile picture"
+              required
+              ref={profilePicture}
+              className="loginInput"
+            />
+
+            <input
+              placeholder="cover Picture"
+              required
+              ref={coverPicture}
+              className="loginInput"
+              type="url"
             />
             <input
               placeholder="Email"
@@ -51,6 +89,18 @@ export default function Register() {
               ref={email}
               className="loginInput"
               type="email"
+            />
+              <input
+                placeholder="Country"
+                required
+                ref={from}
+                className="loginInput"
+              />
+            <input
+              placeholder="City"
+              required
+              ref={city}
+              className="loginInput"
             />
             <input
               placeholder="Password"
@@ -72,7 +122,9 @@ export default function Register() {
               Sign Up
             </button>
 
-            <button className="loginRegisterButton">Log into Account</button>
+            <button className="loginRegisterButton" onClick={reDirectToLogin}>
+              Log into Account
+            </button>
           </form>
         </div>
       </div>
