7cd479a8ef9f9b1148d75f15162c76d0d5614f7b
haimaye074@gmail.com <haimaye074@gmail.com>
2022-03-24 18:41:56 +0200

    footer desc

--- client/src/components/pages/home/Home.jsx
+++ client/src/components/pages/home/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
           <Feed/>
           <Rightbar/>
         </div>
-        {/* <CollapseFooter/> */}
+        <CollapseFooter/>
     </>
   )
 }
--- client/src/components/parts/collapseFooter/CollapseFooter.jsx
+++ client/src/components/parts/collapseFooter/CollapseFooter.jsx
@@ -1,4 +1,6 @@
+import { GitHub, LinkedIn } from "@material-ui/icons";
 import "./collapseFooter.css";
+
 
 
 export default function CollapseFooter() {
@@ -10,8 +12,21 @@ export default function CollapseFooter() {
         <div id="container">
           <div id="cont">
             <div className="footer_center">
-              <h3>Classy footer text</h3>
-
+              <h4>all rights deserve to our Full-Stack developers</h4>
+              {/* <hr></hr> */}
+              <div className="icons">
+                <h4>Eden Genet Tasama</h4>
+            <a href="https://github.com/EdenGenetTasama"><GitHub style={{height:"40px",width:"40px"}}></GitHub></a>
+            <a href="https://www.linkedin.com/in/eden-genet-tasama-8a2a18220/"><LinkedIn style={{height:"40px",width:"40px"}}></LinkedIn></a>
+            <hr></hr>
+            <h4>Amir Mangistu</h4>
+            <a href="https://github.com/Amir-Mangisto"><GitHub style={{height:"40px",width:"40px"}}></GitHub></a>
+            <a href="https://www.linkedin.com/in/amir-mangisto-03a19a226/"><LinkedIn style={{height:"40px",width:"40px"}}></LinkedIn></a>
+            <hr></hr>
+            <h4>Haim Ayenow</h4>
+            <a href="https://github.com/Haim-Ayenow"><GitHub style={{height:"40px",width:"40px"}}></GitHub></a>
+            <a href="https://www.linkedin.com/in/haim-ayenow-0065a4226/"><LinkedIn style={{height:"40px",width:"40px"}}></LinkedIn></a>
+              </div>
             </div>
           </div>
         </div>
