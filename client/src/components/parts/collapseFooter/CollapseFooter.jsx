1cf61fa90745441a6a3943e6656f56f4b4ce4ee6
Amir-Mangisto <mangisto.man@gmail>
2022-03-28 23:05:18 +0300

    changing mylastName

--- client/src/components/parts/collapseFooter/CollapseFooter.jsx
+++ client/src/components/parts/collapseFooter/CollapseFooter.jsx
@@ -17,16 +17,16 @@ export default function CollapseFooter() {
               <hr></hr>
               <div className="icons">
                 <h6>Eden Genet Tasama</h6>
-            <a href="https://github.com/EdenGenetTasama"><GitHub style={{height:"40px",width:"40px"}}></GitHub></a>
-            <a href="https://www.linkedin.com/in/eden-genet-tasama-8a2a18220/"><LinkedIn style={{height:"40px",width:"40px"}}></LinkedIn></a>
+            <a href="https://github.com/EdenGenetTasama" target={"_blank"}><GitHub style={{height:"40px",width:"40px"}}></GitHub></a>
+            <a href="https://www.linkedin.com/in/eden-genet-tasama-8a2a18220/" target={"_blank"}><LinkedIn style={{height:"40px",width:"40px"}}></LinkedIn></a>
             <hr></hr>
-            <h6>Amir Mangistu</h6>
-            <a href="https://github.com/Amir-Mangisto"><GitHub style={{height:"40px",width:"40px"}}></GitHub></a>
-            <a href="https://www.linkedin.com/in/amir-mangisto-03a19a226/"><LinkedIn style={{height:"40px",width:"40px"}}></LinkedIn></a>
+            <h6>Amir Mangisto</h6>
+            <a href="https://github.com/Amir-Mangisto" target={"_blank"}><GitHub style={{height:"40px",width:"40px"}}></GitHub></a>
+            <a href="https://www.linkedin.com/in/amir-mangisto-03a19a226/" target={"_blank"}><LinkedIn style={{height:"40px",width:"40px"}}></LinkedIn></a>
             <hr></hr>
             <h6>Haim Ayenow</h6>
-            <a href="https://github.com/Haim-Ayenow"><GitHub style={{height:"40px",width:"40px"}}></GitHub></a>
-            <a href="https://www.linkedin.com/in/haim-ayenow-0065a4226/"><LinkedIn style={{height:"40px",width:"40px"}}></LinkedIn></a>
+            <a href="https://github.com/Haim-Ayenow" target={"_blank"}><GitHub style={{height:"40px",width:"40px"}}></GitHub></a>
+            <a href="https://www.linkedin.com/in/haim-ayenow-0065a4226/" target={"_blank"}><LinkedIn style={{height:"40px",width:"40px"}}></LinkedIn></a>
               </div>
             </div>
           </div>
